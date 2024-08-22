9c6c63103d4ffb327a1f75d7129cd25f61e57c20
Ahmed Sabry <mohamedmahrous581@gmail.com>
2024-08-22 21:23:51 +0300

    feat: search

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let category = document.querySelector('#category')
 let count = document.querySelector('#count')
 let deleteall = document.querySelector('#deleteall')
 let table = document.querySelector('#table')
+let byname = document.querySelector('#byname')
+let bycategory = document.querySelector('#bycategory')
+let searchbar = document.querySelector('#searchbar')
 
 function totalprice(){
     if (price.value != '') {
@@ -21,7 +24,7 @@ function totalprice(){
 }
 
 let arr = []
-if (JSON.parse(localStorage.products).length > 0) {
+if (localStorage.products!=null && localStorage.products!==undefined && JSON.parse(localStorage.products).length > 0) {
     arr=JSON.parse(localStorage.products)
     deleteall.style.display='block'
 }
@@ -32,33 +35,36 @@ else
 
 submit.onclick= function(){
     let newproduct = {
-        title: title.value,
+        title: title.value.toLowerCase(),
         price: price.value,
         taxes: price.value,
         ads: ads.value,
         discount: discount.value,
         total: total.innerHTML,
         count: count.value,
-        category: category.value
+        category: category.value.toLowerCase()
     }
-    if(submit.innerHTML==='Create'){
-        if(count.value>=1){
-            for (let index = 0; index < count.value; index++) {
+    if (title.value!='' && price.value!=0 && category!='' && count.value<=100) {
+        if(submit.innerHTML=='Create'){
+            if(count.value>1){
+                for (let index = 0; index < count.value; index++) {
+                    arr.push(newproduct)
+                }
+            }
+            else{
                 arr.push(newproduct)
             }
         }
         else{
-            arr.push(newproduct)
+            arr[x]=newproduct
+            submit.innerHTML='Create'
+            count.style.display='block'
         }
+        clear()
     }
-    else{
-        arr[x]=newproduct
-        submit.innerHTML='Create'
-        count.style.display='block'
-    }
+    
     localStorage.products = JSON.stringify(arr)
     deleteall.style.display ='block'
-    clear()
     show()
 }
 
@@ -78,7 +84,7 @@ function show(){
     for (let i = 0; i < arr.length; i++) {
         tabledata +=`
         <tr>
-            <td>${i}</td>
+            <td>${i+1}</td>
             <td>${arr[i].title}</td>
             <td>${arr[i].price}</td>
             <td>${arr[i].taxes}</td>
@@ -92,6 +98,7 @@ function show(){
     `
     }
     table.innerHTML = tabledata
+
 }
 show()
 
@@ -104,6 +111,9 @@ function removeall(){
 function removeitem(index){
     arr.splice(index,1)
     localStorage.products=JSON.stringify(arr)
+    if (arr.length==0) {
+        deleteall.style.display='none'
+    }
     show()
 }
 
@@ -120,4 +130,61 @@ function updateitem(index){
     submit.innerHTML='Update'
     x=index
     scrollTo(0,0)
+}
+
+function searchByName(){
+    if (searchbar.value !== '') {
+        let tabledata = ''
+        for (let index = 0; index < arr.length; index++) {
+            if (searchbar.value.toLowerCase()===arr[index].title) {
+                tabledata +=`
+                <tr>
+                    <td>${index+1}</td>
+                    <td>${arr[index].title}</td>
+                    <td>${arr[index].price}</td>
+                    <td>${arr[index].taxes}</td>
+                    <td>${arr[index].ads}</td>
+                    <td>${arr[index].discount}</td>
+                    <td>${arr[index].total}</td>
+                    <td>${arr[index].category}</td>
+                    <td><button onclick="updateitem(${index})" class="btn btn-primary py-0">Update</button></td>
+                    <td><button onclick="removeitem(${index})" class="btn btn-primary py-0">Delete</button></td>
+                </tr>
+            `
+            }
+            table.innerHTML = tabledata
+        }
+        searchbar.value=''
+    }
+    else{
+        show()
+    }
+}
+function searchByCategory(){
+    if (searchbar.value !== '') {
+        let tabledata = ''
+        for (let index = 0; index < arr.length; index++) {
+            if (searchbar.value.toLowerCase()===arr[index].category) {
+                tabledata +=`
+                <tr>
+                    <td>${index}</td>
+                    <td>${arr[index].title}</td>
+                    <td>${arr[index].price}</td>
+                    <td>${arr[index].taxes}</td>
+                    <td>${arr[index].ads}</td>
+                    <td>${arr[index].discount}</td>
+                    <td>${arr[index].total}</td>
+                    <td>${arr[index].category}</td>
+                    <td><button onclick="updateitem(${index})" class="btn btn-primary py-0">Update</button></td>
+                    <td><button onclick="removeitem(${index})" class="btn btn-primary py-0">Delete</button></td>
+                </tr>
+            `
+            }
+            table.innerHTML = tabledata
+        }
+        searchbar.value=''
+    }
+    else{
+        show()
+    }
 }
